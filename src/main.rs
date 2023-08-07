b170bc114dd726f8fa58708f5a4f3a8eacf0daf4
Hejdula <Hejdula@proton.me>
2023-08-08 00:08:09 +0200

    runtime mutability for game field

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,45 @@
 use clap::Parser;
-
 mod sneko_engine {
-    pub struct GameField {
-        pub data: Box<[Box<[i32]>]>,
-        fruit_count:usize
+    use std::cell::RefCell;
+    pub struct Game {
+        field: RefCell<Box<[Box<[isize]>]>>,
+        fruit_count:usize,
+        snekos:Vec<Sneko>
     }
-    impl GameField{
-        pub fn new(width:usize,heigth:usize,fruit_count:usize) -> GameField {
-            GameField {
-                data: vec![
+    impl Game{
+        pub fn new(width:usize,heigth:usize,fruit_count:usize) -> Game {
+            Game {
+                field: RefCell::new(vec![
                     vec![0;width].into_boxed_slice();heigth
-                ].into_boxed_slice(), 
+                ].into_boxed_slice()), 
                 fruit_count:1,
+                snekos:vec![]
             }
         }
         pub fn print_field(&self){
-            self.data.iter().for_each(|row|println!("{:?}",row));
+            self.field.borrow().iter().for_each(|row|println!("{:?}",row));
         }
-        pub fn slither(){
+        pub fn tick(&mut self){
+            for row in self.field.borrow_mut().iter_mut(){
+                for value in row.iter_mut(){
+                    if *value != 0 {
+                        *value -= 1;
+                    }
+                }     
+            }
+//            self.field.iter().for_each(|row|{
+//                row.iter().for_each(|&value|{
+//                    if value!=0{return value-=1}
+//                })
+//            })
+//            self.field.iter()
+//                .map(|row|row.iter()
+//                     .map(|value|{
+//                         if *value!=0{return *value-=1} else {value}
+//                     }).collect::<&Box<i32>>());
+        }
+        fn spawn_fruit(){
+            //nomnom
         }
     }
     pub enum Direction{
@@ -27,15 +49,24 @@ mod sneko_engine {
         Right
     }
     pub struct Sneko {
-        pub coordinates: (usize,usize)
+        pub coordinates: (usize,usize),
+        pub direction:Direction
+    }
+    impl Sneko {
+        pub fn new(coordinates:(usize,usize))-> Sneko{
+            Sneko { coordinates, direction: (Direction::Right) }    
+        }
+        pub fn slither(){
+            //wiggly wiggle
+        }
     }
 }
 
 
 fn main() {
     let config = Cli::parse();
-    let game_field = sneko_engine::GameField::new(config.width,config.heigth,config.fruit_count);
-    game_field.print_field();
+    let game = sneko_engine::Game::new(config.width,config.heigth,config.fruit_count);
+    game.print_field();
 }
 
 
