250aa6d01884af530398ed0d946030b7d1d68a1a
Hejdula <Hejdula@proton.me>
2023-08-07 21:49:40 +0200

    some sneko fields

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
 use clap::Parser;
 
-mod game_engine {
+mod sneko_engine {
     pub struct GameField {
-        pub data: Box<[Box<[i32]>]>
+        pub data: Box<[Box<[i32]>]>,
+        fruit_count:usize
     }
     impl GameField{
-        pub fn new(width:usize,heigth:usize) -> GameField {
+        pub fn new(width:usize,heigth:usize,fruit_count:usize) -> GameField {
             GameField {
                 data: vec![
                     vec![0;width].into_boxed_slice();heigth
-                ].into_boxed_slice() 
+                ].into_boxed_slice(), 
+                fruit_count:1,
             }
         }
         pub fn print_field(&self){
             self.data.iter().for_each(|row|println!("{:?}",row));
         }
+        pub fn slither(){
+        }
+    }
+    pub enum Direction{
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    pub struct Sneko {
+        pub coordinates: (usize,usize)
     }
 }
 
 
 fn main() {
     let config = Cli::parse();
-    let game_field = game_engine::GameField::new(config.width,config.heigth);
+    let game_field = sneko_engine::GameField::new(config.width,config.heigth,config.fruit_count);
     game_field.print_field();
 }
 
@@ -36,4 +49,6 @@ struct Cli {
     width: usize,
     #[arg(short='y', long, default_value_t=10)]
     heigth: usize,
+    #[arg(short='f', long, default_value_t=1)]
+    fruit_count:usize,
 }
