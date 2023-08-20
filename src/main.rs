b1c79d0a7dd8473dac6520b92ec4206710b91904
Hejdula <Hejdula@proton.me>
2023-08-21 00:45:49 +0200

    removed refcells...

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use sneko_engine::Game;
 use std::error::Error;
 use std::thread::sleep;
 use std::time::Duration;
@@ -15,12 +16,20 @@ fn main() {
 
 fn run(cli:Cli)-> Result<(),Box<dyn Error>>{
     let game_config=sneko_engine::Config::new(cli.width,cli.heigth,cli.fruit_count);
-    let game = sneko_engine::Game::new(game_config);
+    let mut game = sneko_engine::Game::new(game_config);
     game.add_player("Hejdula".to_string(),"soket".to_string());
-    (1..13).for_each(|_|{game.tick();println!("");sleep(Duration::from_secs(1))});
+    game.add_player("2".to_string(),"soket".to_string());
+    game.add_player("3".to_string(),"soket".to_string());
+    game.add_player("4".to_string(),"soket".to_string());
+    game.add_player("5".to_string(),"soket".to_string());
+    game.start();
+    (1..2).for_each(|_|{game.tick();println!("");sleep(Duration::from_millis(400))});
     return Ok(());
 }
 
+fn game_loop(game:Game){
+    
+}
 
 
 #[derive(Parser)]
--- src/sneko_engine.rs
+++ src/sneko_engine.rs
@@ -20,25 +20,25 @@ pub enum Point {
 }
 pub struct Game {
     soket: String,
-    pub field: RefCell<Box<[Box<[Point]>]>>,
+    pub field: Box<[Box<[Point]>]>,
     config: Config,
-    snekos:RefCell<Vec<Sneko>>,
-    fruits:RefCell<Vec<Coords>>
+    snekos:Vec<Sneko>,
+    fruits:Vec<Coords>
 }
 impl Game{
     pub fn new(config:Config) -> Game {
         Game {
             soket: String::new(),
-            field: RefCell::new(vec![
+            field: vec![
                 vec![Point::Empty;config.field_width].into_boxed_slice();config.field_heigth
-            ].into_boxed_slice()), 
+            ].into_boxed_slice(), 
             config,
-            snekos:RefCell::new(vec![]),
-            fruits:RefCell::new(vec![])
+            snekos:vec![],
+            fruits:vec![]
         }
     }
     pub fn print_field(&self){
-        self.field.borrow().iter().for_each(|row|{ 
+        self.field.iter().for_each(|row|{ 
             println!("");
             row.iter().for_each(|point|{
                 match point {
@@ -49,10 +49,63 @@ impl Game{
             })
         })
     }
-    pub fn start(&self){
-
+    pub fn start(&mut self){
+        self.spawn_snekos();
+        //self.spawn_fruits();
+        self.update_field_values();
     }
-    pub fn tick(&self){
+    pub fn spawn_snekos(&mut self){
+        let number_of_snekos = self.snekos.len();
+        self.snekos.iter_mut().enumerate().for_each(|(i,snek)|{
+            snek.alive=true;
+            let side_direction = match i%4{
+                0 => {Direction::Left}
+                1 => {Direction::Right}
+                2 => {Direction::Up}
+                3 => {Direction::Down}
+                _ => unreachable!()
+            };
+            snek.prev_direction = side_direction.opposite();
+            snek.direction = side_direction.opposite();
+            let snekos_on_side = (number_of_snekos/4) + {
+                match i%4 <= number_of_snekos%4 {
+                    true => {1}
+                    false => {0}
+                }
+            };
+            let nth_on_side=1+(i/4);
+            let spacing = self.config.field_width as f32 / ((snekos_on_side+1) as f32);
+            snek.coords = {match side_direction {
+                Direction::Up => {
+                    Coords{x:(nth_on_side as f32 * spacing).ceil() as usize,y:self.config.field_heigth-1}}
+                Direction::Down => {
+                    Coords{x:(nth_on_side as f32 * spacing).ceil() as usize,y:0}}
+                Direction::Left => {
+                    Coords{x:0,y:(nth_on_side as f32 * spacing).ceil() as usize}}
+                Direction::Right => {
+                    Coords{x:self.config.field_width-1,y:(nth_on_side as f32 * spacing).ceil() as usize}}
+            }};
+            println!("{:?},snekos on side:{},nth_on_side:{},{},{:?}",side_direction,snekos_on_side,nth_on_side,spacing,snek.coords)
+        });
+    }
+        
+    fn update_field_values(&mut self){ 
+        self.snekos.iter().for_each(|snek|{
+            if let true = snek.alive{
+                self.field[snek.coords.y][snek.coords.x] = Point::Sneko(snek.length);    
+            }
+       })
+    }
+
+    pub fn change_player_direction(&mut self,nickname:String,direction:Direction){
+        //let reference_to_sneko: Option<&Sneko> = self.snekos.iter().find(|snek| snek.nickname == nickname);
+        //if let Some(snek) = self.snekos.iter().find(|snek| snek.nickname == nickname){
+        //    if snek.prev_direction.opposite() != direction{
+        //        snek.direction = direction;
+        //    }
+        //}
+    }
+    pub fn tick(&mut self){
         if self.number_of_alive_snekos() == 0 {
             println!("GAME OVER");
             return
@@ -63,35 +116,35 @@ impl Game{
         self.decrease_field_values();
         self.slither_alive_snekos();
         self.handle_movement();
+        print! ("\x1B[2J\x1B[1;1H");
         self.print_field();
     }
     fn number_of_alive_snekos(&self) -> usize{
         let mut result:usize = 0;
-        self.snekos.borrow().iter().for_each(|snek|if let true = *snek.alive.borrow(){result+=1});
+        self.snekos.iter().for_each(|snek|if let true = snek.alive{result+=1});
         result
     }
-    fn handle_movement(&self){ 
-        self.snekos.borrow().iter().for_each(|snek|{
+    fn handle_movement(&mut self){ 
+        self.snekos.iter_mut().for_each(|sneko|{
             let mut dies = false;
-            match *snek.alive.borrow(){
+            match sneko.alive{
                 true => {
-                    let snek_coords = snek.coords.borrow_mut();
-                    let point = &mut self.field.borrow_mut()[snek_coords.y][snek_coords.x];
+                    let point = &mut self.field[sneko.coords.y][sneko.coords.x];
                     match point{
                         Point::Empty=>{
-                            *point = Point::Sneko(*snek.length.borrow())}
+                            *point = Point::Sneko(sneko.length)}
                         Point::Fruit => {
-                            *point = Point::Sneko(*snek.length.borrow())}
+                            *point = Point::Sneko(sneko.length)}
                         Point::Sneko(_) => {dies = true;}
                     }
                 }
                 false => {}
             }
-            if dies {*snek.alive.borrow_mut()=false}
+            if dies {sneko.alive = false}
         })  
     }
-    fn decrease_field_values(&self){
-        self.field.borrow_mut().iter_mut().for_each(|row|{
+    fn decrease_field_values(&mut self){
+        self.field.iter_mut().for_each(|row|{
             row.iter_mut().for_each(|point|{
                 if let Point::Sneko(value) = point {
                     match value {
@@ -102,41 +155,53 @@ impl Game{
             })
         })
     }
-    fn slither_alive_snekos(&self){    
-        self.snekos.borrow().iter().for_each(|snek|{ 
-            if *snek.alive.borrow() == false {return}
+    fn slither_alive_snekos(&mut self){    
+        self.snekos.iter_mut().for_each(|snek|{ 
+            if snek.alive == false {return}
             snek.slither();
-            let snek_coords = snek.coords.borrow();
-            if snek_coords.x>=self.config.field_width || snek_coords.y>=self.config.field_heigth{    
-                *snek.alive.borrow_mut() = false;
+            if snek.coords.x>=self.config.field_width || snek.coords.y>=self.config.field_heigth{    
+                snek.alive = false;
             }
         })
     }
-    fn spawn_fruits(&self){
-        while self.fruits.borrow().len() < self.config.fruit_count{
+    fn spawn_fruits(&mut self){
+        while self.fruits.len() < self.config.fruit_count{
             let mut rnd = rand::thread_rng();
             let rand_coords = Coords{
                 x: rnd.gen_range(0..self.config.field_width),
                 y: rnd.gen_range(0..self.config.field_heigth)
             };
-            if let Point::Fruit=self.field.borrow()[rand_coords.y][rand_coords.x]{
-                self.fruits.borrow_mut().push(rand_coords)
+            if let Point::Fruit=self.field[rand_coords.y][rand_coords.x]{
+                self.fruits.push(rand_coords)
             }
         }
         //nomnom
     }
-    pub fn add_player(&self,nickname:String,player_soket:String){
-        self.snekos.borrow_mut().push(Sneko::new(nickname,player_soket))
+    pub fn add_player(&mut self,nickname:String,player_soket:String){
+        self.snekos.push(Sneko::new(nickname,player_soket))
+    }
+    pub fn remove_player(&self,nickname:String){
+        
     }
 }
-
+#[derive(PartialEq,Debug)]
 pub enum Direction{
     Up,
     Down,
     Left,
     Right
 }
-#[derive(Debug)]
+impl Direction{
+    pub fn opposite(&self)->Direction{
+        match self{
+            Direction::Up=>{Direction::Down}
+            Direction::Down=>{Direction::Up}
+            Direction::Left=>{Direction::Right}
+            Direction::Right=>{Direction::Left}
+        }
+    }
+}
+#[derive(Clone,Debug)]
 pub struct Coords {
     pub x:usize,
     pub y:usize,
@@ -145,11 +210,12 @@ pub struct Coords {
 pub struct Sneko {
     nickname:String,
     player_soket: String,
-    pub coords: RefCell<Coords>,
-    pub direction: RefCell<Direction>,
-    pub body_cells: RefCell<Vec<Coords>>,
-    pub length: RefCell<usize>,
-    pub alive: RefCell<bool>
+    pub coords: Coords,
+    pub direction: Direction,
+    pub body_cells: Vec<Coords>,
+    pub length: usize,
+    pub alive: bool,
+    pub prev_direction: Direction
 }
 
 impl Sneko {
@@ -157,28 +223,28 @@ impl Sneko {
         Sneko {
             nickname,
             player_soket,
-            coords:RefCell::new(Coords{x:0,y:0}), 
-            direction: RefCell::new(Direction::Right),
-            body_cells: RefCell::new(vec![]),//yet to implement
-            length: RefCell::new(3),
-            alive:RefCell::new(true)}    
+            coords: Coords{x:0,y:0}, 
+            direction: Direction::Right,
+            body_cells: vec![],//yet to implement
+            length: 3,
+            alive: true,    
+            prev_direction: Direction::Right
+        }
     }
-    pub fn slither(&self){
-        match *self.direction.borrow() {
-            Direction::Up => {self.coords.borrow_mut().y+=1},
+    pub fn slither(&mut self){
+        match self.direction {
+            Direction::Up => {self.coords.y+=1},
             Direction::Down =>{  
-                let mut coords_borrow = self.coords.borrow_mut(); 
-                match self.coords.borrow().y {
-                    0 => {*self.alive.borrow_mut() = false}
-                    _ => {coords_borrow.y-=1}
+                match self.coords.y {
+                    0 => {self.alive = false}
+                    _ => {self.coords.y-=1}
                 } },
             Direction::Left =>{ 
-                let mut coords_borrow = self.coords.borrow_mut(); 
-                match coords_borrow.x {
-                    0 => {*self.alive.borrow_mut() = false}
-                    _ => {coords_borrow.x-=1}
+                match self.coords.x {
+                    0 => {self.alive = false}
+                    _ => {self.coords.x-=1}
                 }}
-            Direction::Right =>{self.coords.borrow_mut().x+=1},
+            Direction::Right =>{self.coords.x+=1},
         }
         //wiggly wiggle
     }
